5ace0048e36e5b6bf3fbbb55d8796db3f58dfff0
Quimorax <nikitademch24@gmail.com>
2026-09-09 22:28:36 +0200

    feat: add base logic of manual ship placement

--- src/InputParser.java
+++ src/InputParser.java
@@ -42,7 +42,41 @@ public class InputParser {
         return size;
     }
 
-    public Position parseShipPlacement(int shipPlacement) {
+    public int[] parseShipPlacement(int shipSize, int fieldSize) {
+        System.out.println("Enter placement for your ship with size " + shipSize + " in format A4 H (or E7 V): ");
+        String input = scanner.nextLine();
 
+        String[] values = input.split(" ");
+        if (values.length != 2) {
+            throw new ParseException("You have typed more than 2 objects");
+        }
+
+        Position position = parsePosition(values[0], fieldSize);
+
+        String value = values[1].toUpperCase();
+        if (!value.equals("H") && value.equals("V")) {
+            throw new ParseException("");
+        }
+
+        Orientation orientation = value.equals("H") ? Orientation.HORIZONTAL : Orientation.VERTICAL;
+
+        return new int[]{position.row(), position.col(), orientation.ordinal()};
+    }
+
+    public Position parsePosition(String input, int fieldSize) {
+        if (input.length() != 2) {
+            throw new ParseException("");
+        }
+        if (input.charAt(0) < 'A' || input.charAt(0) > 'A' + fieldSize - 1) {
+            throw new ParseException("");
+        }
+        if (input.charAt(1) < '1' || input.charAt(1) > '1' + fieldSize - 1) {
+            throw new ParseException("");
+        }
+
+        int row = 'A' - input.charAt(0);
+        int col = '1' - input.charAt(1);
+
+        return new Position(row, col);
     }
 }
--- src/ManualShipPlacer.java
+++ src/ManualShipPlacer.java
@@ -9,6 +9,30 @@ public class ManualShipPlacer extends ShipPlacer {
 
     @Override
     public void placeShips(PlayerField field, List<Integer> shipSizes) {
+        // TODO: add feature, that you init also List<Ship> list
+        for (int size : shipSizes) {
+            Position position;
+            Orientation orientation;
+            int[] values;
 
+            while (true) {
+                try {
+                    values = parser.parseShipPlacement(size, field.getSize());
+                } catch (ParseException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Enter one more time");
+                    continue;
+                }
+                position = new Position(values[0], values[1]);
+                orientation = Orientation.values()[values[2]];  // ??
+
+                if (isValid(field, position, orientation, size)) {
+                    break;
+                }
+            }
+
+            // fill in into place this this...
+
+        }
     }
 }
