ae3c5ee5159b84242d2cde6ada344deea0447ee7
Quimorax <nikitademch24@gmail.com>
2026-09-17 17:53:02 +0200

    feat(PositionUtils): add class with position utils

--- src/PositionUtils.java
+++ src/PositionUtils.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class PositionUtils {
+    public static Position shift(Position position, Orientation orientation, int shift) {
+        if (orientation == Orientation.HORIZONTAL) {
+            return new Position(position.row(), position.col() + shift);
+        }
+        return new Position(position.row() + shift, position.col());
+    }
+
+    public static Position shift(Position position, int rowShift, int columnShift) {
+        return new Position(position.row() + rowShift, position.col() + columnShift);
+    }
+
+    public static boolean isInRange(Position position, int fieldSize) {
+        int row = position.row();
+        int col = position.col();
+
+        return row >= 0 && row < fieldSize && col >= 0 && col < fieldSize;
+    }
 }
