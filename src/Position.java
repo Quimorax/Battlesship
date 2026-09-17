617752e56fc70740eb8e4cf1cdca15e3ef913563
Quimorax <nikitademch24@gmail.com>
2026-09-17 17:53:52 +0200

    feat(Position): remove coordinates checking from class

--- src/Position.java
+++ src/Position.java
@@ -1,7 +1,1 @@
-public record Position(int row, int col) {
-    public Position {
-        if (row < 0 || col < 0) {
-            throw new IllegalArgumentException("Coordinates can't be < 0");
-        }
-    }
-}
+public record Position(int row, int col) {}
