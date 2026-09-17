3f409cb25c77f5c6ed23a1e5bf6d51818d244580
Quimorax <nikitademch24@gmail.com>
2026-09-17 17:51:26 +0200

    feat: implement needed additional ship validation logic

--- src/ShipPlacer.java
+++ src/ShipPlacer.java
@@ -1,32 +1,20 @@
 import java.util.*;
 
 public abstract class ShipPlacer {
-    private static final List<Integer> DEFAULT_SHIP_SIZES = List.of(4, 3, 3, 2, 2, 2, 1, 1, 1, 1);
+    private static final List<Integer> DEFAULT_SHIP_SIZES = List.of(4, 3, 2, 1);
 
     public abstract void placeShips(PlayerField field, List<Integer> shipSizes);
 
     protected boolean isValid(PlayerField field, Position position, Orientation orientation, int shipSize) {
-        int row = position.row();
-        int col = position.col();
-
-        Position shiftedPosition;
-        int changedValue;
+        // row -- number, col -- letter
         for (int shift = 0; shift < shipSize; shift++){
-            if (orientation == Orientation.HORIZONTAL) {
-                changedValue = col + shift;
-                shiftedPosition = new Position(row, col + shift);
-            } else {
-                changedValue = row + shift;
-                shiftedPosition = new Position(row + shift, col);
-            }
-            if (changedValue < 0 || changedValue >= field.getSize()) {
+            Position shiftedPosition = PositionUtils.shift(position, orientation, shift);
+            if (!PositionUtils.isInRange(shiftedPosition, field.getSize())) {
                 return false;
             }
-
-            if (field.getCell(shiftedPosition) != PlayerField.CellStatus.EMPTY) {
+            if (field.getCell(shiftedPosition) != PlayerField.CellStatus.EMPTY || isShipNear(shiftedPosition, field)) {
                 return false;
             }
-            shipSize--;
         }
         return true;
     }
@@ -35,13 +23,37 @@ public abstract class ShipPlacer {
         List<Position> positions = new ArrayList<>();
 
         for (int i = 0; i < shipSize; i++) {
-            Position currentPosition = orientation == Orientation.HORIZONTAL ? new Position(position.row(), position.col() + i) : new Position(position.row() + i, position.col());
+            Position currentPosition = PositionUtils.shift(position, orientation, i);
             positions.add(currentPosition);
             field.setCell(currentPosition, PlayerField.CellStatus.SHIP);
         }
 
         Ship ship = new Ship(positions);
         field.addSHip(ship);
+    }
+
+    private boolean isShipNear(Position position, PlayerField field) {
+        int[][] shifts = new int[][]{
+                {1, 1},
+                {-1, -1},
+                {1, -1},
+                {-1, 1},
+                {1, 0},
+                {0, 1},
+                {-1, 0},
+                {0, -1}
+        };
+
+        for (int[] shift : shifts) {
+            Position shiftedPosition = PositionUtils.shift(position, shift[0], shift[1]);
+            if (PositionUtils.isInRange(shiftedPosition, field.getSize())) {
+                if (field.getCell(shiftedPosition) == PlayerField.CellStatus.SHIP) {
+                    return true;
+                }
+            };
+        }
+
+        return false;
     }
 
     public static List<Integer> getDefaultShipSizes() {
